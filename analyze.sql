990ce466db61624effa1cadf9aff6942caf0e86d
tomohxx <43703429+tomohxx@users.noreply.github.com>
2021-06-30 16:19:43 +0000

    プレイヤ名を出力するように変更

--- analyze.sql
+++ analyze.sql
@@ -1,17 +1,15 @@
 \echo *************************************************************
-\echo *** Mjai Recoder Report (`date`) ***
+\echo * Mjai Recoder Report (`date`)
+\echo * :NAME
 \echo *************************************************************
 
 \pset footer
 
-create view my_winnings(actor, target) as select actor, target from winnings inner join players on winnings.actor = players.id where players.name = :'NAME';
-create view my_ryukyokus(tenpai) as select tenpai from ryukyokus inner join players on ryukyokus.player_id = players.id where players.name = :'NAME';
-
 select count(*) as 試合数 from games inner join players on games.id = players.game_id where players.name = :'NAME';
 select count(*) as 局数 from rounds inner join players on rounds.game_id = players.game_id where players.name = :'NAME';
 \gset
-select round(count(*)::numeric/:局数, 3) as 和了率 from my_winnings;
-select round(count(*)::numeric/(select count(*) from my_winnings), 3) as 自摸率 from my_winnings where my_winnings.actor = my_winnings.target;
+select round(count(*)::numeric/:局数, 3) as 和了率 from winnings inner join players on winnings.actor = players.id where players.name = :'NAME';
+select round(count(*)::numeric/(select count(*) from winnings inner join players on winnings.actor = players.id where players.name = :'NAME'), 3) as 自摸率 from winnings inner join players on winnings.actor = players.id where players.name = :'NAME' and winnings.actor = winnings.target;
 select round(avg(winnings.delta), 0) as 平均得点 from winnings inner join players on winnings.actor = players.id where players.name = :'NAME';
 select round(avg(winnings.delta), 0) as 平均失点 from winnings inner join players on winnings.actor != players.id and winnings.target = players.id where players.name = :'NAME';
 select round(count(*)::numeric/:局数, 3) as 放銃率 from winnings inner join players on winnings.target = players.id where winnings.actor != players.id and players.name = :'NAME';
@@ -20,7 +18,4 @@ select round(count(*)::numeric/:局数, 3) as 副露率 from (select distinct na
 select ranking.position as 順位, count(ranking.position) as 回数 from (select results.position from results inner join players on results.player_id = players.id where players.name = :'NAME') as ranking group by ranking.position order by ranking.position;
 select round(avg(results.position), 3) as 平均順位 from results inner join players on results.player_id = players.id where players.name = :'NAME';
 select round(count(*)::numeric/:局数, 3) as 流局率 from ryukyokus inner join players on ryukyokus.player_id = players.id where players.name = :'NAME';
-select round(count(*)::numeric/(select count(*) from my_ryukyokus), 3) as 流局時聴牌率 from my_ryukyokus where my_ryukyokus.tenpai;
-
-drop view my_winnings;
-drop view my_ryukyokus;
+select round(count(*)::numeric/(select count(*) from ryukyokus inner join players on ryukyokus.player_id = players.id where players.name = :'NAME'), 3) as 流局時聴牌率 from ryukyokus inner join players on ryukyokus.player_id = players.id where players.name = :'NAME' and ryukyokus.tenpai;
