b843a30066615d5a705619a8a2aaf0d3f626259d
tomohxx <43703429+tomohxx@users.noreply.github.com>
2021-12-07 02:08:36 +0900

    フロントエンドを修正

--- web/vue/src/main.js
+++ web/vue/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
